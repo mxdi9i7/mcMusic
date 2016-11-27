415ce30391780ce9d295a1cfe8a4020fbcd3104e
mxdi9i7 <peter.zheng88228@gmail.com>
2016-11-27 02:33:44 -0500

    node commonjs modules

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
+var math = require("./math.js");
+
 var command = process.argv[2];
 var a = Number(process.argv[3]);
 var b = Number(process.argv[4]);
 
-if (command === "add") {
-	console.log(a+b);
-} else if (command === "multiply") {
-	console.log(a * b);
-} else if (command === "divide") {
-	console.log(a / b);
-} else if (command === "subtract") {
-	console.log(a - b);
-}
+var value = math[command](a, b);
+
+console.log("The result of " + a + " " + command + " " + b + " is " + value);
---
+++ math.js
@@ -0,0 +1,20 @@
+module.exports = {
+	add: function(a, b) {
+		return a + b;
+	},
+	subtract: function(a, b) {
+		return a - b;
+	}
+}
+
+
+
+// if (command === "add") {
+// 	console.log(a+b);
+// } else if (command === "multiply") {
+// 	console.log(a * b);
+// } else if (command === "divide") {
+// 	console.log(a / b);
+// } else if (command === "subtract") {
+// 	console.log(a - b);
+// }
